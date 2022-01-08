1d8ae23535cb47c8fcf5d3448e345e3d53605feb
Utsav Baghela <utsavbaghela.ub@gmail.com>
2022-01-09 02:16:29 +0530

    Update E-13. Roman to Integer.cpp

--- E-13. Roman to Integer.cpp
+++ E-13. Roman to Integer.cpp
@@ -4,63 +4,22 @@ class Solution {
 public:
     int romanToInt(string s) {
         int sum=0;
-        for(int i=0;i<s.size();i++)
+        unordered_map<char, int> m = { { 'I' , 1 },
+                                       { 'V' , 5 },
+                                       { 'X' , 10 },
+                                       { 'L' , 50 },
+                                       { 'C' , 100 },
+                                       { 'D' , 500 },
+                                       { 'M' , 1000 } };
+                                   
+        for(int i=0;i<s.size()-1;i++)
         {   
-            if(s[i]=='I' && s[i+1]=='V'){
-                sum+=4;
-                i++;continue;
-            }
-            
-            if(s[i]=='I' && s[i+1]=='X'){
-                sum+=9;
-                i++;continue;
-            }
-
-            
-            if(s[i]=='X' && s[i+1]=='L'){
-                sum+=40;
-                i++;continue;
-            }
-
-            if(s[i]=='X' && s[i+1]=='C'){
-                sum+=90;
-                i++;continue;
-            }
-
-            
-            if(s[i]=='C' && s[i+1]=='D'){
-                sum+=400;
-                i++;continue;
-            }
-
-            if(s[i]=='C' && s[i+1]=='M'){
-                sum+=900;
-                i++;continue;
-            }
-
-            if(s[i]=='M'){
-                sum+=1000;
-            }
-            if(s[i]=='D'){
-                sum+=500;
-            }
-            if(s[i]=='C'){
-                sum+=100;
-            }
-            if(s[i]=='L'){
-                sum+=50;
-            }
-            if(s[i]=='X'){
-                sum+=10;
-            }
-            if(s[i]=='V'){
-                sum+=5;
-            }
-            if(s[i]=='I'){
-                sum+=1;
-            }
-            
+          if(m[s[i]] < m[s[i+1]])
+              sum=sum-m[s[i]];
+          else
+              sum=sum+m[s[i]];
         }
+        sum=sum+m[s[s.size()-1]];
         return sum;
     }
 };
